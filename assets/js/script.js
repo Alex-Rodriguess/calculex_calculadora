acfad333fb538649503860f9c0663b688820e30b
unknown <alexmusicalsa@gmail.com>
2022-10-16 17:09:41 -0300

    nova calculex

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+function inserir(n) {
+  var num = document.getElementById("display").innerHTML;
+  document.getElementById("display").innerHTML = num + n;
+}
+function limpar() {
+  document.getElementById("display").innerHTML = "";
+}
+function calcular() {
+  var res = document.getElementById("display").innerHTML;
+  document.getElementById("display").innerHTML = eval(res);
+}
+function voltar() {
+  var res = document.getElementById("display").innerHTML;
+  document.getElementById("display").innerHTML = res.substring(
+    0,
+    res.length - 1
+  );
+}
